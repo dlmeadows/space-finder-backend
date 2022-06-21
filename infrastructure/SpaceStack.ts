7ff63e8db3e3689cd82d5a806c9ce603b4d94f9f
dmeadows <meadows.david@siemens.com>
2022-06-21 13:23:57 -0400

    Testing and debugging Lambdas

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -5,6 +5,8 @@ import {GenericTable} from './GenericTable';
 import {Function as LambdaFunction, Runtime, Code} from 'aws-cdk-lib/aws-lambda';
 import {LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway';
 import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
+import {PolicyStatement} from 'aws-cdk-lib/aws-iam'
+
 
 export class SpaceStack extends Stack {
 
@@ -18,19 +20,17 @@ export class SpaceStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props);
 
-        const helloLambda = new LambdaFunction(this, 'helloLambda', {
-            runtime: Runtime.NODEJS_16_X,
-            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-            handler: 'hello.main'
-        });
-
         const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
             entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
             handler: 'handler',
-        })
+        });
+        const s3ListPolicy = new PolicyStatement();
+        s3ListPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListPolicy.addResources('*');
+        helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
 
         // Hello Api lambda integration:
-        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
         const helloLambdaResource = this.api.root.addResource('hello');
         helloLambdaResource.addMethod('GET', helloLambdaIntegration);
     }
